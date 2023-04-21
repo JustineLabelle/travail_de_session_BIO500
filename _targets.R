2387718dd15685aec8474fcc92fc93fd6420e48f
Kayla_Trempe-Kay <kaylatrempekay14@gmail.com>
2023-04-21 17:19:37 -0400

    Supprimer les scripts figurer 1, 2 et 3 et nettoyage du script targets

--- _targets.R
+++ _targets.R
@@ -2,25 +2,11 @@ library(targets)
 library(tarchetypes)
 library(RSQLite)
 
-
 source("scripts/nettoyage2.R")
 
 source("scripts/requete2.R")
 
 source("scripts/figurer.R")
-
-
-#source("scripts/figurer.R")
-#source("scripts/collaboration.R")
-#source("scripts/cour.R")
-#source("scripts/etudiant.R")
-#source("scripts/manipulations.R")
-#source("scripts/roquaite.R")
-#source("scripts/figurer.R")
-#source("rapport/rapport.Rmd")
-#source("scripts/figurer1.R")
-#source("scripts/figurer2.R")
-#source("scripts/figurer3.R")
 
 tar_option_set(packages = c("knitr","RSQLite","vioplot","igraph","scales"))
 
@@ -48,22 +34,7 @@ list(
     command = figurer(roquaite)
  ),
   tar_render(
-    name = test,
-    path ="rapport_final/rapport_final.Rmd"
-    #library = rticles,
-     #output_format="rticles::pnas_article"
-))
+    name = rapport, 
+    path ="rapport/rapport.Rmd"
 
-#)#,
-#  tar_target(
-#    name = clean_courtarget, # Cible pour l'exécution de la figure
-#    command = cour(rawdatatarget)
-#  ),
-#  tar_target(
-#   name = clean_etudianttarget,
-#    command = etudiant(rawdatatarget)
-# ),
-#  tar_target(
-#   name = rawdatatarget,
-#  command = rawdata(file_paths) #¸étape prep donné dans target cours
-# ),
+))
